557635ab38ba36ebbdf77fb50865540dd017853d
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-10-07 10:19:17 +0300

    creating a button in our navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,4 +29,16 @@ const Navbar = () => {
           <Link className="navbar-brand" to="/">
             Habari Sasa
           </Link>
+          
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
