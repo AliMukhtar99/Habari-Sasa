0528dcb3bccd275981755ba219964d65e745d549
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-10-07 10:21:07 +0300

    completing necessary elements in navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,8 +15,8 @@ const Navbar = () => {
     });
   }, []);
 
-   // This function will scroll the window to the top
-   const scrollToTop = () => {
+  // This function will scroll the window to the top
+  const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth", // for smoothly scrolling
@@ -29,7 +29,7 @@ const Navbar = () => {
           <Link className="navbar-brand" to="/">
             Habari Sasa
           </Link>
-          
+
           <button
             className="navbar-toggler"
             type="button"
@@ -42,3 +42,65 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/business" className="nav-link">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/entertainment" className="nav-link">
+                  Entertainment
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  Technology
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div
+          className="d-flex justify-content-end sticky-top"
+          style={{ padding: "5px" }}
+        >
+          {showButton && (
+            <button
+              onClick={scrollToTop}
+              type="button"
+              className="btn btn-primary btn-lg"
+            >
+              ☝️
+            </button>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
