bd8e735910f4596126f9287dbc9a4b69cb802984
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-10-07 10:06:24 +0300

    starting up with Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
